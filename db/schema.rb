e568aed01f5910e21cdb380e9e21dbfd79f47456
David Montante <davidmontante@gmail.com>
2019-05-15 00:56:33 +0000

    Changes database for heroku deploy

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20190513184840) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "group_events", force: :cascade do |t|
     t.string   "name"
     t.string   "location"
